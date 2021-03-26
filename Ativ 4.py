475d5834361484618bbe136dca8868081dc066ce
RafaelMarcolinoSilva <77708442+RafaelMarcolinoSilva@users.noreply.github.com>
2021-03-26 16:39:44 -0300

    Add files via upload
    Este pequeno código em Python mostra como receber 4 números e retorna a sua média aritmética.

---
+++ Ativ 4.py
@@ -0,0 +1,8 @@
+print("Cálculo de média aritmética")#título do programa
+num1=int(input("Insira o primeiro Número:\n" ))#entrada do primeiro e demais números
+num2=int(input("Insira o segundo Número:\n" ))
+num3=int(input("Insira o terceiro Número:\n" ))
+num4=int(input("Insira o quarto Número:\n" ))
+
+media= (num1+num2+num3+num4) / 4 #fórmula que realiza o cálculo de média
+print(f"O resultado da média aritmética é {media}!!!") #impressão do resultado do cálculo da média
